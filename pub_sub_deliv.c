1e35b80e290e8a33615204dc69fbe16afb564aaf
Niklas <niklas@debbrecht.com>
2019-04-09 16:37:15 +0200

    created new files

--- pub_sub_deliv.c
+++ pub_sub_deliv.c
@@ -1,62 +1,6 @@
-/*
- * rls_client.c - remote directory-listing client
- * Quelle Bloomer, Power RPC Programming
- * Adaptiert von C. Westerkamp WS2009/10
- * getestet unter Ubuntu 10.04 32Bit
- */
-#include <stdio.h>
-#include <rpc/rpc.h>
-#include <errno.h>
-#include "rls.h"
+#include "pub_sub_deliv.h"
 
-extern int errno;
-
-int main(int argc, char *argv[])
-{
-    CLIENT *cl;
-    char *server;
-    char *dir;
-    readdir_res *result;
-    namelist nl;
-
-    if( argc != 3 ) {
-        fprintf(stderr, "usage: %s host directory\n", argv[0]);
-        exit(1);
-    }
-
-    /* Command line arguments */
-    server = argv[1];
-    dir = argv[2];
-
-    /* Client handle erzeugen (mit TCP Protokoll) */
-    cl = clnt_create(server, DIRPROG, DIRVERS, "tcp");
-    if( cl == NULL ) {
-        /* Verbindungsaufbau erfolglos  */
-        clnt_pcreateerror(server);
-        exit(1);
-    }
-    /* Eigentlicher Aufruf der entfernten Prozedur */ 
-    result = readdir_1(&dir, cl);
-    if( result == NULL ) {
-        /* Fehler beim RPC-Aufruf */
-        clnt_perror(cl, server);
-        exit(1);
-    }
-    /* RPC-Aufruf erfolgreich, Resultat auswerten */
-    if( result->remoteErrno != 0 ) { 
-        /* RPC-Fehler aus Server */
-        errno = result->remoteErrno;
-        perror(dir);
-        exit(1);
-    }
-    /* Resultat ohne Fehler, Verzeichnisliste ausgeben */
-    for( nl=result->readdir_res_u.list;
-        nl != NULL;
-        nl = nl->pNext) {
-        
-        printf("%s\n", nl->name);
-    }
-    exit(0);
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
 }
-
-
