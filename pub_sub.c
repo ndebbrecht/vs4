8b86f55faa1fb44634c226dcd89ff0e45ed0ecf4
Niklas <niklas@debbrecht.com>
2019-04-10 09:00:43 +0200

    updates

--- pub_sub.c
+++ pub_sub.c
@@ -2,19 +2,23 @@
 #include <stdio.h>
 #include <rpc/rpc.h>
 #include "pub_sub.h"
+#include "pub_sub_deliv.h"
 
-CLIENT subscribed[512];
+char *subscribed[512];
 unsigned short subCounter = 0;
 topic *channel;
+struct scv_req;
 
 int main(int argc, char const *argv[]) {
 
   return 0;
 }
 
-short *unsubscribe_1_svc(topic *tp, svc_req *req){
+short *unsubscribe_1_svc(void *t, struct svc_req *req){
   for(unsigned short i = 0; i <= subCounter; i++){
-    if(subscribed[i] == req->rq_xprt->xp_raddr.sin_addr.s_addr){
+    char *address;
+    sprintf(address, "%i",req->rq_xprt->xp_raddr.sin_addr.s_addr);
+    if(strcmp(subscribed[i], address)){
       subscribed[i] = subscribed[subCounter];
       subCounter--;
       return 0;
@@ -22,18 +26,23 @@ short *unsubscribe_1_svc(topic *tp, svc_req *req){
   }
 }
 
-short *subscribe_1_svc(topic *tp, svc_req *req){
-  subscribed[subCounter] = req->rq_xprt->xp_raddr.sin_addr.s_addr;
+short *subscribe_1_svc(void *t, struct svc_req *req){
+  char *address;
+  sprintf(address, "%i",req->rq_xprt->xp_raddr.sin_addr.s_addr);
+  subscribed[subCounter] = address;
   subCounter++;
   return 0;
 }
 
-short *set_channel_1_svc(topic *tp, svc_req *req){
+short *set_channel_1_svc(topic *tp, struct svc_req *req){
   channel = tp;
 }
 
-short *publish_1_svc(message *message, svc_req *req){
+short *publish_1_svc(message *message, struct svc_req *req){
   for(unsigned short i = 0; i <= subCounter; i++){
-    deliver_1(message, subscribed[i]);
+    CLIENT *cl;
+    cl = clnt_create(subscribed[i], PUBSUBPROG, PUBSUBVERS, "tcp");
+    postmessage *m = message;
+    deliver_1(m, cl);
   }
 }
