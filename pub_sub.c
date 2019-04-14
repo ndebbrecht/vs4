d4c0e3f046e65178264332c771b69ff4e5df2ee2
Niklas <niklas@debbrecht.com>
2019-04-14 14:59:37 +0200

    nvhsddgs

--- pub_sub.c
+++ pub_sub.c
@@ -50,7 +50,9 @@ short *publish_1_svc(message *message, struct svc_req *req){
   for(unsigned short i = 0; i < subCounter; i++){
     CLIENT *cl;
     cl = clnt_create(subscribed[i], PUBSUBCLTPROG, PUBSUBCLTVERS, "tcp");
-    postmessage *m = message;
+    postmessage *m = topic;
+    strcat(m, ": ");
+    strcat(m, message);
     printf("send message; %s\n", *message);
     deliver_1(m, cl);
     printf("sent\n");
